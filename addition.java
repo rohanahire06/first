1a8639ecd8ce02c331afeab585bfa373d68fcb46
rohanahire06 <rohanahire06@gmail.com>
2023-02-03 20:53:19 +0530

    Addition of two numbers

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class addition {
+    public static int addNumbers(int a, int b) {
+      return a + b;
+    }
+    
+    public static void main(String[] args) {
+      int x = 5, y = 10;
+      int sum = addNumbers(x, y);
+      System.out.println("The sum of " + x + " and " + y + " is: " + sum);
+    }
+  }
+  
